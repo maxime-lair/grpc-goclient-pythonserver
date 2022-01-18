cab80f6f1b24e05bfd6968fdead78a61b8da673b
maxime-lair <72258375+maxime-lair@users.noreply.github.com>
2022-01-18 19:00:27 +0100

    add background run for server

--- start.sh
+++ start.sh
@@ -32,7 +32,7 @@ python3 -m grpc_tools.protoc -Iprotos/ --python_out=pythonserver/ --grpc_python_
 
 printf "Starting python server\n"
 # Start server
-./pythonserver/serve.py
+./pythonserver/serve.py &
 server_pid=$!
 printf "Server started with PID %s\n" "$server_pid"
 
