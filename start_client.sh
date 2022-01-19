d6845d872e5a1d158fd7e06e2385b83f43565b4f
maxime-lair <72258375+maxime-lair@users.noreply.github.com>
2022-01-19 19:32:30 +0100

    add protoc

--- start_client.sh
+++ start_client.sh
@@ -39,6 +39,12 @@ else
 fi
 
 printf "Compiling protos files\n"
+# Require protobuf compiler installed
+# PROTOC_ZIP=protoc-3.14.0-linux-x86_64.zip
+# curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
+# sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+# sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
+# rm -f $PROTOC_ZIP
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ../protos/server.proto
 
 printf "Starting go client\n"
