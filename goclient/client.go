de1f7cc09c5d45b273e6974cc5b73a37cc5a1f7d
maxime-lair <72258375+maxime-lair@users.noreply.github.com>
2022-01-20 23:56:11 +0100

    test random

--- goclient/client.go
+++ goclient/client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"log"
+	"math/rand"
+	"os"
 
 	pb "main/pb_server"
 
@@ -13,6 +16,46 @@ import (
 var (
 	serverAddr = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
 )
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func get_random_line(open_file *os.File) string {
+
+	random_line := ""
+
+	var lines []string
+	scanner := bufio.NewScanner(open_file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	line_number := rand.Intn(len(lines))
+
+	log.Printf("Picking line random %d among total %d\n", line_number, len(lines))
+
+	return random_line
+}
+
+func define_client_id() string {
+	// Get random line from a color wordlist
+	color_file, color_err := os.Open("../worldlist/color.txt")
+	check(color_err)
+	defer color_file.Close()
+	color_picked := get_random_line(color_file)
+	// Get random line from an animal wordlist
+	animal_file, animal_err := os.Open("../worldlist/animal.txt")
+	check(animal_err)
+	defer animal_file.Close()
+	animal_picked := get_random_line(animal_file)
+
+	// return both string concat
+	log.Printf("Created new client id : %s - %s\n", color_picked, animal_picked)
+	return color_picked + "_" + animal_picked
+}
 
 func main() {
 	flag.Parse()
@@ -31,6 +74,8 @@ func main() {
 
 	client := pb.NewSocketGuideClient(conn)
 
-	log.Printf("Created client %s", client)
+	client_id := &pb.SocketTree{Name: define_client_id()}
+
+	log.Printf("Created client %v with id %s", client, client_id)
 
 }
