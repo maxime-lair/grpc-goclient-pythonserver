281c585f196ea3e08f2303bfd55fa5962e35c9ad
maxime-lair <72258375+maxime-lair@users.noreply.github.com>
2022-01-22 16:52:02 +0100

    test with updated model

--- goclient/client.go
+++ goclient/client.go
@@ -21,6 +21,122 @@ var (
 	serverAddr = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
 )
 
+type conn_info struct {
+	opts       []grpc.DialOption
+	serverAddr string
+}
+
+type model struct {
+	choices  []string         // items on the to-do list
+	cursor   int              // which to-do list item our cursor is pointing at
+	selected map[int]struct{} // which to-do items are selected
+
+	socketFamilyChoices   []*pb.SocketFamily   // socket family choices list
+	socketTypeChoices     []*pb.SocketType     // socket type choices list
+	socketProtocolChoices []*pb.SocketProtocol // socket protocol choices list
+	selectedFamily        *pb.SocketFamily     // selected family
+	selectedType          *pb.SocketType       // selected type
+	selectedProtocol      *pb.SocketProtocol   // selected protocol
+
+	logJournal []string // log journal
+}
+
+/*************
+Bubbletea part
+*************/
+
+func initialModel(connInfo conn_info) model {
+
+	log.Printf("Connected to server, proceeding..")
+
+	return model{
+		// Our shopping list is a grocery list
+		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+
+		// A map which indicates which choices are selected. We're using
+		// the  map like a mathematical set. The keys refer to the indexes
+		// of the `choices` slice, above.
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+	s := "Select your choice.\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
+}
+
 /*************
 GRPC part
 *************/
@@ -120,19 +236,29 @@ func socket_get_protocol_list(socketTypeAndFamilyChoice *pb.SocketTypeAndFamily,
 
 func main() {
 
-	p := tea.NewProgram(initialModel())
-	if err := p.Start(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
-
+	// Retrieve connections options (TLS, etc..)
 	flag.Parse()
 	var opts []grpc.DialOption
-
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	log.Printf("Connecting to: %s", *serverAddr)
+	/* Start TUI */
+	if err := tea.NewProgram(initialModel(conn_info{serverAddr: *serverAddr, opts: opts})).Start(); err != nil {
+		log.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+	/* End of TUI */
 
+	/* Connect to server message (loading bar) */
+
+	/* Update with family list */
+
+	/* Show family choice, update with type list */
+
+	/* Show family/type choice, update with protocol list */
+
+	/* Show family/type/protocol choice */
+
+	log.Printf("Connecting to: %s", *serverAddr)
 	conn, err := grpc.Dial(*serverAddr, opts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
