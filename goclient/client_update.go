14cfe6e50ba789eb6c28c787cb279c43d6b3aae0
maxime-lair <72258375+maxime-lair@users.noreply.github.com>
2022-01-24 21:42:47 +0100

    after

--- goclient/client_update.go
+++ goclient/client_update.go
@@ -75,6 +75,7 @@ func (m model) UpdateConnect(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) UpdateGetFamily(msg tea.Msg) (tea.Model, tea.Cmd) {
 
+	m.clientEnv.logJournal = append(m.clientEnv.logJournal, fmt.Sprintf("After Stopwatch: %v, test %v\n", m.stopwatch, m.stopwatch.Running()))
 	switch msg := msg.(type) {
 
 	// Is it a key press?
